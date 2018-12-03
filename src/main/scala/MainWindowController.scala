37b612b379a1634a9f8846415a65f53f188c0f4c
markaiv <anthonymarkiv@gmail.com>
2018-12-03 14:50:50 +0800

    fffffff

--- src/main/scala/MainWindowController.scala
+++ src/main/scala/MainWindowController.scala
@@ -7,4 +7,6 @@ class MainWindowController(private val image_banner: ImageView) {
   //val file = getClass.getResourceAsStream("Window.fxml")
   //val images = getClass.getResourceAsStream("images/uno.png")
   //val image: Image = ImageIO.read(getClass.getClassLoader.getResource("images/uno.png"))
+
+  //Another test
 }
