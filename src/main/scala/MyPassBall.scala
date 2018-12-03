88922412641662a374daebb897a69690ef8c3ffc
markaiv <anthonymarkiv@gmail.com>
2018-12-03 13:28:59 +0800

    Test

--- src/main/scala/MyPassBall.scala
+++ src/main/scala/MyPassBall.scala
@@ -16,7 +16,7 @@ import scalafx.scene.control.Alert.AlertType
 object MyPassBall extends JFXApp{
 
   val clients = new ObservableHashSet[ActorRef]()
-
+  //Test Mk
 
   var count = -1
   val addresses = (for (inf <- NetworkInterface.getNetworkInterfaces.asScala;
